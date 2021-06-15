bf86fdb61d5f253b4629f77e624e6d56c2a4bd27
Tenzin Sean <tenzin.jeunesse@gmail.com>
2021-06-16 00:35:38 +0200

    added features cart

--- src/components/QuantitySelector/index.tsx
+++ src/components/QuantitySelector/index.tsx
@@ -1,40 +1,27 @@
 import React from 'react';
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
 const QuantitySelector = ({quantity, setQuantity}) => {
-
   const onMinus = () => {
     setQuantity(Math.max(0, quantity - 1));
-  }
+  };
 
   const onPlus = () => {
     setQuantity(quantity + 1);
-  }
+  };
 
   return (
     <View style={styles.root}>
-      <Pressable
-        onPress={onMinus}
-        style={styles.button}
-      >
-        <Text
-          style={styles.buttonText}
-        >-</Text>
+      <Pressable onPress={onMinus} style={styles.button}>
+        <Text style={styles.buttonText}>-</Text>
       </Pressable>
-      <Text
-        style={styles.quantity}
-      >{quantity}</Text>
+      <Text style={styles.quantity}>{quantity}</Text>
 
-      <Pressable
-        onPress={onPlus}
-        style={styles.button}
-      >
-        <Text
-          style={styles.buttonText}
-        >+</Text>
+      <Pressable onPress={onPlus} style={styles.button}>
+        <Text style={styles.buttonText}>+</Text>
       </Pressable>
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
@@ -43,9 +30,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 1,
     borderColor: '#e3e3e3',
-    justifyContent: 'space-between',
+    justifyContent: 'space-between',
     width: 130,
-
   },
   button: {
     width: 35,
@@ -55,11 +41,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#d1d1d1',
   },
   buttonText: {
-    fontSize: 18
+    fontSize: 18,
   },
   quantity: {
-    color: '#007eb9'
-  }
-})
+    color: '#007eb9',
+  },
+});
 
 export default QuantitySelector;
--- src/screens/ShoppingCartScreen/index.tsx
+++ src/screens/ShoppingCartScreen/index.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
-import ProductItem from '../../components/ProductItems';
+import {View, Text, StyleSheet, Image, FlatList} from 'react-native';
+import CartProductItem from '../../components/CartProductItem';
 import QuantitySelector from '../../components/QuantitySelector';
 import products from '../../data/cart';
+import Button from '../../components/Button';
 
 const ShoppingCart = () => {
+  const totalPrice = products.reduce(
+    (summedPrice, product) =>
+      summedPrice + product.item.price * product.quantity,
+    0,
+  );
+
   return (
     <View style={styles.page}>
-      {/* Render Product Compoentn */}
       {/* <ProductItem item={products[1]} /> */}
 
       <FlatList
         data={products}
-        renderItem={({ item }) => (
-          <ProductItem item={item.item} />
-          
-        )}
+        renderItem={({item}) => <CartProductItem cartItem={item} />}
         showsVerticalScrollIndicator={false}
-        keyExtractor={({ id }) => id}
+        ListHeaderComponent={() => (
+          <View>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              Subtotal ({products.length} items):{' '}
+              <Text style={{color: '#e47911', fontWeight: 'bold'}}>
+                ${totalPrice.toFixed(2)}
+              </Text>
+            </Text>
+            <Button
+              text="Processed to checkout"
+              onPress={() => console.warn('got')}
+              containerStyles={{
+                backgroundColor: '#f7e300',
+                borderColor: '#c7b602',
+              }}
+            />
+          </View>
+        )}
+        // keyExtractor={({ id }) => id}
       />
     </View>
   );
@@ -26,7 +47,6 @@ const ShoppingCart = () => {
 const styles = StyleSheet.create({
   page: {
     padding: 10,
-
   },
 });
 
