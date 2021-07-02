39661ecd535faaaafd96dd1572bc26cfadc9b2d8
Tenzin Sean <tenzin.jeunesse@gmail.com>
2021-07-02 03:19:32 +0200

    add view

--- src/screens/AddressScreen/index.tsx
+++ src/screens/AddressScreen/index.tsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react'
-import { View, Text, TextInput,Button} from 'react-native'
+import { View, Text, TextInput, Alert, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
 import { Picker } from '@react-native-picker/picker';
 import countryList from 'country-list';
 import styles from './styles';
+import Button from '../../components/Button';
 
 const countries = countryList.getData();
 
@@ -11,20 +12,49 @@ const AddressScreen = () => {
   const [fullname, setFullname] = useState('');
   const [phone, setPhonenumber ] = useState('');
   const [address, setAddress ] = useState('');
+  const [addressError, setAddressError] = useState('Invalid add');
   const [city, setCity ] = useState('');
+
+  // const {state, setState} = useState({
+  //   country: '',
+  //   fullname: '',
+  // })
+
 
   console.log(fullname);
 
   const onCheckout = () => {
+    if(addressError) {
+      Alert.alert('Fix all field errors before submitting');
+    }
+
     if(!fullname) {
-      Alert.alert('Pleasse fill in the fu');
+      Alert.alert('Pleasse fill in the fullname');
       return;
     }
-    console.warn('Success down');
+
+    if(!phone) {
+      Alert.alert('Please fill phone number');
+      return;
+    }
+    console.warn('Success. Checkout ');
+  }
+
+  const validateAddress = () => {
+    if (address.length < 3) {
+      setAddressError('Address is too short');
+    }
+
+    if(address.length > 10) {
+      setAddressError('Address is too long');
+    }
   }
 
   return (
-    <View style={styles.root}>
+    <KeyboardAvoidingView
+       behavoir={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+    <ScrollView style={styles.root}>
       <View style={styles.row}>
         <Picker
          onValueChange={setCountry}
@@ -64,8 +94,15 @@ const AddressScreen = () => {
         style={styles.input}
         placeholder="Address"
         value={address}
-        onChangeText={setAddress}
+        onEndEditing={validateAddress}
+        onChangeText={(text) => {
+          setAddress(text);
+          setAddressError('')
+        }}
         />
+        {!!addressError && (
+          <Text style={styles.errorLabel}>{addressError}</Text>
+        )}
       </View>
 
        {/* Address */}
@@ -77,11 +114,47 @@ const AddressScreen = () => {
         value={city}
         onChangeText={setCity}
         />
+        
+      </View>
+
+      {/* Phone number */}
+       <View style={styles.row}>
+        <Text style={styles.label}>Phone number </Text>
+        <TextInput 
+        style={styles.input}
+        placeholder="Phone number"
+        value={phone}
+        onChangeText={setPhonenumber}
+        keyboardType={'phone-pad'}
+        />
+      </View>
+
+      {/* Phone number */}
+       <View style={styles.row}>
+        <Text style={styles.label}>Phone number </Text>
+        <TextInput 
+        style={styles.input}
+        placeholder="Phone number"
+        value={phone}
+        onChangeText={setPhonenumber}
+        keyboardType={'phone-pad'}
+        />
+      </View>
+      {/* Phone number */}
+       <View style={styles.row}>
+        <Text style={styles.label}>Phone number </Text>
+        <TextInput 
+        style={styles.input}
+        placeholder="Phone number"
+        value={phone}
+        onChangeText={setPhonenumber}
+        keyboardType={'phone-pad'}
+        />
       </View>
 
       <Button text='Checkout' onPress={onCheckout} />
-      
-    </View>
+    </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
